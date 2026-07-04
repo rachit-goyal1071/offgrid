43fda65dc9e3b84dcc63648f5e8dfb3026dc9e98
rachitgoyal <rachit@hudle.in>
2026-07-04 21:19:42 +0530

    Initial Commit

---
+++ OffGrid/OffGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffGridApp.swift
+//  OffGrid
+//
+//  Created by Rachit Goyal on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct OffGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
